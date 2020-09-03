814bd74cf78f1556117e7d7621dbc9daab39a5a9
Reni Dantas <renidantas0@gmail.com>
2020-09-03 01:51:07 -0300

    Initial commit

---
+++ AuthSystem/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualBasic;
+
+namespace AuthSystem.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string Nome { get; set; }
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string Sobrenome { get; set; }
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string Cpf { get; set; }
+        [Column(TypeName = "Date")]
+        public DateTime DataNascimento { get; set; }
+        [Column(TypeName = "int")]
+        public int Nivel { get; set; }
+    }
+}
---
+++ AuthSystem/Areas/Identity/Data/MyUserClaimsPrincipalFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Areas.Identity.Data
+{
+    public class MyUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser>
+    {
+        public MyUserClaimsPrincipalFactory(UserManager<ApplicationUser> userMan,
+            IOptions<IdentityOptions> optionsAcessor) : base(userMan, optionsAcessor)
+        {
+
+        }
+
+        protected override async Task<ClaimsIdentity>GenerateClaimsAsync(ApplicationUser user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("Nome", user.Nome));
+            return identity;
+        }
+    }
+}
